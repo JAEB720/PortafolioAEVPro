fdf63c93bafb4b63a3dfd2364448539d09a95714
JAEB720 <jeuescobarbernudez@gmail.com>
2024-04-27 13:31:39 -0600

    PFp.V.0.0.6

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,6 +3,8 @@ import { Container, Card, Grid, Image, Segment, Button } from 'semantic-ui-react
 import 'react-awesome-slider/dist/styles.css';
 import AwesomeSlider from 'react-awesome-slider';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import { color } from 'three/examples/jsm/nodes/Nodes.js';
+import { Color } from 'three';
 
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 
@@ -13,7 +15,7 @@ function Skills() {
         <Grid.Row style={{ margin: '0rem' }}>
           <Grid.Column computer={8} mobile={16}>
 
-            <Card fluid className="animated-card" style={{ border: "none", padding: "0.5rem",}}>
+            <Card fluid className="animated-car" style={{ border: "none", padding: "0.5rem",}}>
               <Card.Content style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7))', }}>
                 <Card.Header>Front-end</Card.Header>
 
@@ -29,33 +31,33 @@ function Skills() {
           play={true}
           cancelOnInteraction={false} 
           interval={5000}
-          style={{ width: '100%', height: '3.5rem', margin:"0rem", background: 'linear-gradient(rgba(229, 255, 0, 0.1), rgba(100, 100, 100, 0.1))', border: "none", padding:"0rem",boxShadow: "0 4px 8px rgba(0, 0, 0, 0.9)" }}
+          style={{ width: '100%', height: '4rem', margin:"0rem", background: 'linear-gradient(rgba(229, 255, 0, 0.1), rgba(100, 100, 100, 0.1))', border: "none", padding:"0rem",boxShadow: "0 4px 8px rgba(0, 0, 0, 0.9)" }}
         >
           <div>
-            <Card  >
+            <Card  style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }} >
               <Card.Content textAlign='center' > 
-                <Card.Description  ><Button >
-                  <Image  src="/skills/html.png" alt="Logo 1" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }} />
+                <Card.Description  ><Button fluid style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+                  <Image  src="/skills/html.png" alt="Logo 1" style={{ maxWidth: '15%', maxHeight: '10%',}} />
                   HTML</Button>
                 </Card.Description>
               </Card.Content>
             </Card>
           </div>
           <div>
-            <Card>
+            <Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }}>
               <Card.Content>
-                <Card.Description> <Button>
-                  <Image src="/skills/css.png" alt="Logo 2" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }} />
+                <Card.Description> <Button fluid style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+                  <Image src="/skills/css.png" alt="Logo 2" style={{ maxWidth: '15%', maxHeight: '10%',}} />
                  CSS</Button>
                 </Card.Description>
               </Card.Content>
             </Card>
           </div>
           <div>
-            <Card>
+            <Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }}>
               <Card.Content>
-                <Card.Description>  <Button>
-                  <Image src="/skills/js.png" alt="Logo 3" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }}/>
+                <Card.Description>  <Button fluid style={{background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))',display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+                  <Image src="/skills/js.png" alt="Logo 3" style={{ maxWidth: '15%', maxHeight: '10%', }}/>
                 JavaScript</Button>
                 </Card.Description>
               </Card.Content>
@@ -63,10 +65,10 @@ function Skills() {
           </div>
 
           <div>
-            <Card>
+            <Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }}>
               <Card.Content>
-                <Card.Description> <Button>
-                  <Image src="/skills/react.png" alt="Logo 4" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }} />
+                <Card.Description> <Button fluid style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+                  <Image src="/skills/react.png" alt="Logo 4" style={{ maxWidth: '15%', maxHeight: '10%',}} />
                  React</Button>
                 </Card.Description>
               </Card.Content>
@@ -86,7 +88,7 @@ function Skills() {
 
 <Grid.Column computer={8} mobile={16}>
 
-<Card fluid className="animated-card" style={{ border: "none", padding: "0.5rem",}}>
+<Card fluid className="animated-car" style={{ border: "none", padding: "0.5rem",}}>
   <Card.Content style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7))', }}>
     <Card.Header>Back-end</Card.Header>
 
@@ -102,23 +104,23 @@ virtualize={true}
 play={true}
 cancelOnInteraction={false} 
 interval={5000}
-style={{ width: '100%', height: '3.5rem', margin:"0rem", background: 'linear-gradient(rgba(229, 255, 0, 0.1), rgba(100, 100, 100, 0.1))', border: "none", padding:"0rem",boxShadow: "0 4px 8px rgba(0, 0, 0, 0.9)" }}
+style={{ width: '100%', height: '4rem', margin:"0rem", background: 'linear-gradient(rgba(229, 255, 0, 0.1), rgba(100, 100, 100, 0.1))', border: "none", padding:"0rem",boxShadow: "0 4px 8px rgba(0, 0, 0, 0.9)" }}
 >
 <div>
-<Card >
+<Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }} >
   <Card.Content > 
-    <Card.Description  >  <Button>
-      <Image  src="/skills/node.png" alt="Logo 1" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.5rem" }}/>
+    <Card.Description  >  <Button fluid style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+      <Image  src="/skills/node.png" alt="Logo 1" style={{ maxWidth: '15%', maxHeight: '10%',  }}/>
     Node</Button>
     </Card.Description>
   </Card.Content>
 </Card>
 </div>
 <div>
-<Card>
+<Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }}>
   <Card.Content>
-    <Card.Description><Button>
-      <Image src="/skills/express.png" alt="Logo 2" style={{ maxWidth: '15%', maxHeight: '5%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.5rem" }}/>
+    <Card.Description><Button style={{background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+      <Image src="/skills/express.png" alt="Logo 2" style={{ maxWidth: '15%', maxHeight: '15%', }}/>
       Express</Button>
     </Card.Description>
   </Card.Content>
@@ -140,7 +142,7 @@ style={{ width: '100%', height: '3.5rem', margin:"0rem", background: 'linear-gra
 
 <Grid.Column computer={8} mobile={16}>
 
-<Card fluid className="animated-card" style={{ border: "none", padding: "0.5rem", marginTop:"1rem"}}>
+<Card fluid className="animated-car" style={{ border: "none", padding: "0.5rem", marginTop:"1rem"}}>
   <Card.Content style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7))', }}>
     <Card.Header>Languages</Card.Header>
 
@@ -156,38 +158,38 @@ virtualize={true}
 play={true}
 cancelOnInteraction={false} 
 interval={5000}
-style={{ width: '100%', height: '3.5rem', margin:"0rem", background: 'linear-gradient(rgba(229, 255, 0, 0.1), rgba(100, 100, 100, 0.1))', border: "none", padding:"0rem",boxShadow: "0 4px 8px rgba(0, 0, 0, 0.9)" }}
+style={{ width: '100%', height: '4rem', margin:"0rem", background: 'linear-gradient(rgba(229, 255, 0, 0.1), rgba(100, 100, 100, 0.1))', border: "none", padding:"0rem",boxShadow: "0 4px 8px rgba(0, 0, 0, 0.9)" }}
 >
 <div >
-<Card >
+<Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }} >
   <Card.Content > 
-    <Card.Description  > <Button>
-      <Image  src="/skills/js.png" alt="Logo 1" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }} />
+    <Card.Description   > <Button fluid style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+      <Image  src="/skills/js.png" alt="Logo 1" style={{ maxWidth: '15%', maxHeight: '10%',  }} />
      JavaScript</Button>
     </Card.Description>
   </Card.Content>
 </Card>
 </div>
 <div>
-<Card>
+<Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }}>
   <Card.Content>
-    <Card.Description>  <Button>
-      <Image src="/skills/python.png" alt="Logo 2" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }}/>
+    <Card.Description>  <Button fluid style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+      <Image src="/skills/python.png" alt="Logo 2" style={{ maxWidth: '15%', maxHeight: '10%', }}/>
     Python</Button>
     </Card.Description>
   </Card.Content>
 </Card>
 </div>
-<div>
-<Card>
+{/* <div>
+<Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }}>
   <Card.Content>
-    <Card.Description><Button>
-      <Image src="/skills/js.png" alt="Logo 3" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }} />
+    <Card.Description><Button fluid style={{ display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', }}>
+      <Image src="/skills/js.png" alt="Logo 3" style={{ maxWidth: '15%', maxHeight: '10%',  }} />
      JavaScript</Button> 
     </Card.Description>
   </Card.Content>
 </Card>
-</div>
+</div> */}
 
 
 </AutoplaySlider>
@@ -199,7 +201,7 @@ style={{ width: '100%', height: '3.5rem', margin:"0rem", background: 'linear-gra
 
 <Grid.Column computer={8} mobile={16}>
 
-<Card fluid className="animated-card" style={{ border: "none", padding: "0.5rem", marginTop:"1rem"}}>
+<Card fluid className="animated-car" style={{ border: "none", padding: "0.5rem", marginTop:"1rem"}}>
   <Card.Content style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7))', }}>
     <Card.Header>Technologies</Card.Header>
 
@@ -215,33 +217,33 @@ virtualize={true}
 play={true}
 cancelOnInteraction={false} 
 interval={5000}
-style={{ width: '100%', height: '3.5em', margin:"0rem", background: 'linear-gradient(rgba(229, 255, 0, 0.1), rgba(100, 100, 100, 0.1))', border: "none", padding:"0rem",boxShadow: "0 4px 8px rgba(0, 0, 0, 0.9)" }}
+style={{ width: '100%', height: '4rem', margin:"0rem", background: 'linear-gradient(rgba(229, 255, 0, 0.1), rgba(100, 100, 100, 0.1))', border: "none", padding:"0rem",boxShadow: "0 4px 8px rgba(0, 0, 0, 0.9)" }}
 >
 <div>
-<Card >
+<Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }}>
   <Card.Content > 
-    <Card.Description  ><Button>
-      <Image  src="/skills/semantic.png" alt="Logo 1" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }} />
+    <Card.Description  ><Button fluid style={{background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+      <Image  src="/skills/semantic.png" alt="Logo 1" style={{ maxWidth: '15%', maxHeight: '10%', }} />
       React Semantic ui</Button>
     </Card.Description>
   </Card.Content>
 </Card>
 </div>
 <div>
-<Card>
+<Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }}>
   <Card.Content>
-    <Card.Description><Button>
-      <Image src="/skills/opencv.png" alt="Logo 2" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }} />
+    <Card.Description><Button fluid style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+      <Image src="/skills/opencv.png" alt="Logo 2" style={{ maxWidth: '15%', maxHeight: '10%',}} />
       OpenCV</Button>
     </Card.Description>
   </Card.Content>
 </Card>
 </div>
 <div>
-<Card>
+<Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }}>
   <Card.Content>
-    <Card.Description> <Button>
-      <Image src="/skills/three.png" alt="Logo 3" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }} />
+    <Card.Description> <Button fluid style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+      <Image src="/skills/three.png" alt="Logo 3" style={{ maxWidth: '13%', maxHeight: '10%', }} />
      Three</Button>
     </Card.Description>
   </Card.Content>
@@ -249,10 +251,10 @@ style={{ width: '100%', height: '3.5em', margin:"0rem", background: 'linear-grad
 </div>
 
 <div>
-<Card>
+<Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }}>
   <Card.Content>
-    <Card.Description> <Button>
-      <Image src="/skills/tailwind.png" alt="Logo 3" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }} />
+    <Card.Description> <Button fluid style={{background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))',display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+      <Image src="/skills/tailwind.png" alt="Logo 3" style={{ maxWidth: '15%', maxHeight: '10%',margin:"0.3rem"}} />
      Tailwind</Button>
     </Card.Description>
   </Card.Content>
@@ -260,10 +262,10 @@ style={{ width: '100%', height: '3.5em', margin:"0rem", background: 'linear-grad
 </div>
 
 <div>
-<Card>
+<Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }}>
   <Card.Content>
-    <Card.Description> <Button>
-      <Image src="/skills/npm.png" alt="Logo 3" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }} />
+    <Card.Description> <Button fluid style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))',display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+      <Image src="/skills/npm.png" alt="Logo 3" style={{ maxWidth: '20%', maxHeight: '10%', margin:"0.3rem"}} />
      NPM</Button>
     </Card.Description>
   </Card.Content>
@@ -271,10 +273,10 @@ style={{ width: '100%', height: '3.5em', margin:"0rem", background: 'linear-grad
 </div>
 
 <div>
-<Card>
+<Card style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', }}>
   <Card.Content>
-    <Card.Description> <Button>
-      <Image src="/skills/git.png" alt="Logo 4" style={{ maxWidth: '15%', maxHeight: '10%', display:"flex", flexDirection: 'column',  justifyContent: 'center', alignItems: 'center', marginLeft:"6.2rem" }} />
+    <Card.Description> <Button fluid style={{ background: 'linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))', display:"flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color:"white" }}>
+      <Image src="/skills/git.png" alt="Logo 4" style={{ maxWidth: '14%', maxHeight: '10%',margin:"0.2rem" }} />
      Git</Button>
     </Card.Description>
   </Card.Content>
